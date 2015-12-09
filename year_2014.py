5e38afddff52244cdca5f343cc24673f78c6c917
opor-jitpanu <opor_tan@hotmail.co.th>
2015-12-09 21:52:49 +0700

    add

--- year_2014.py
+++ year_2014.py
@@ -1,4 +1,12 @@
 def year_2014():
+    #แบบแรก
     list_1 = [['2014-12-30'], ['Yemen'], ['Ash Shihr'], ['Al-Qa`ida in the Arabian Peninsula (AQAP)'], ['0'], ['0'], ['Police,Business']]
-    
+    #แบบสอง
+    date=['2014-12-30', '2014-12-30', '2014-12-31']
+    county =['yemen','yemen','Democratic Republic of the Congo']
+    city = ['Ash Shihr','Ash Shihr','Dungu district']
+    group = ['Al-Qa`ida in the Arabian Peninsula (AQAP)','Al-Qa`ida in the Arabian Peninsula (AQAP)','Lord s Resistance Army (LRA)']
+    fata = ['0','0','0']
+    inj = ['0','0','2']
+    target = ['Police,Business','Police,Business','Police,Business']
 year_2014()
