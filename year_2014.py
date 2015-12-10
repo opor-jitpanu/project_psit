0cd65c7f0f8ce5aa2c94e22be575e293ff3bdb8f
opor-jitpanu <opor_tan@hotmail.co.th>
2015-12-11 00:59:18 +0700

    year_2014

---
+++ year_2014.py
@@ -0,0 +1,9 @@
+def year_2014_12(text):
+    
+    
+    date = ['2014-12-16', '2014-12-16', '2014-12-16', '2014-12-16', '2014-12-10', '2014-12-09', '2014-12-06', '2014-12-05', '2014-12-04', '2014-12-04', '2014-12-04', '2014-12-01', '2014-12-01', '2014-12-01', '2014-12-01', '2014-12-01']
+    city = ['Bachodistrict', 'Bachodistrict', 'Bachodistrict', 'Kabangdistrict', 'Riang', 'Mayo', 'BannangSata', 'ThaKamcham', 'MuangYaladistrict', 'BanKhaoDin', 'BangoSato', 'Yupo', 'Unknown', 'Unknown', 'Sawo', 'PlongHoi']
+    per = ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown']
+    fata = ['0', '0', '0', '0', '0', '1', '0', '0', '1', '1', '1', '0', '0', '0', '0', '0']
+    inju = ['0', '0', '0', '0', '3', '0', '2', '0', '1', '0', '0', '0', '2', '0', '0', '0']
+    target = ['Police,Police', 'Police', 'PrivateCitizens&Property', 'Police', 'Military', 'Government(General)', 'Police', 'Military,EducationalInstitution', 'Police', 'Government(General)', 'PrivateCitizens&Property', 'Police', 'Business', 'Police', 'Business', 'Government(General)']
