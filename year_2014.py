e7683a9951ac116bd94e866b3f8a0755759b2723
opor-jitpanu <opor_tan@hotmail.co.th>
2015-12-11 02:23:22 +0700

    edit

--- year_2014.py
+++ year_2014.py
@@ -1,12 +1,16 @@
 def year_2014_12():
     
     
-    date = ['2014-12-16', '2014-12-16', '2014-12-16', '2014-12-16', '2014-12-10', '2014-12-09', '2014-12-06', '2014-12-05', '2014-12-04', '2014-12-04', '2014-12-04', '2014-12-01', '2014-12-01', '2014-12-01', '2014-12-01', '2014-12-01']
-    city = ['Bachodistrict', 'Bachodistrict', 'Bachodistrict', 'Kabangdistrict', 'Riang', 'Mayo', 'BannangSata', 'ThaKamcham', 'MuangYaladistrict', 'BanKhaoDin', 'BangoSato', 'Yupo', 'Unknown', 'Unknown', 'Sawo', 'PlongHoi']
-    per = ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown']
+    date = ['2014-12-16', '2014-12-16', '2014-12-16', '2014-12-16', '2014-12-10', '2014-12-09', '2014-12-06', '2014-12-05', '2014-12-04', '2014-12-04', '2014-12-04', '2014-12-01', '2014-12-01', \
+            '2014-12-01', '2014-12-01', '2014-12-01']
+    city = ['Bachodistrict', 'Bachodistrict', 'Bachodistrict', 'Kabangdistrict', 'Riang', 'Mayo', 'BannangSata', 'ThaKamcham', \
+            'MuangYaladistrict', 'BanKhaoDin', 'BangoSato', 'Yupo', 'Unknown', 'Unknown', 'Sawo', 'PlongHoi']
+    per = ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', \
+           'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown']
     fata = ['0', '0', '0', '0', '0', '1', '0', '0', '1', '1', '1', '0', '0', '0', '0', '0']
     inju = ['0', '0', '0', '0', '3', '0', '2', '0', '1', '0', '0', '0', '2', '0', '0', '0']
-    target = ['Police,Police', 'Police', 'PrivateCitizens&Property', 'Police', 'Military', 'Government(General)', 'Police', 'Military,EducationalInstitution', 'Police', 'Government(General)', 'PrivateCitizens&Property', 'Police', 'Business', 'Police', 'Business', 'Government(General)']
+    target = ['Police,Police', 'Police', 'PrivateCitizens&Property', 'Police', 'Military', 'Government(General)', 'Police',\
+              'Military,EducationalInstitution', 'Police', 'Government(General)', 'PrivateCitizens&Property', 'Police', 'Business', 'Police', 'Business', 'Government(General)']
     inj = 0
     fat = 0
     list_test = []
@@ -18,12 +22,18 @@ def year_2014_12():
     return all_list
 
 def year_2014_11():
-    date = ['2014-11-27', '2014-11-25', '2014-11-18', '2014-11-17', '2014-11-15', '2014-11-15', '2014-11-15', '2014-11-12', '2014-11-11', '2014-11-11', '2014-11-10', '2014-11-10', '2014-11-10', '2014-11-05', '2014-11-03', '2014-11-02', '2014-11-02', '2014-11-01']
-    city = ['TanyongTalo', 'Waeng', 'RuamThaiPattana', 'SiSakhon', 'BanBluKa', 'KhokPhodistrict', 'Betongdistrict', 'Aiyoeweng', 'KhaoTum', 'BannangSatadistrict', 'Ruesodistrict', 'ChoengKhiri', 'Wat', 'SungaiPadidistrict', 'Talubo', 'Muangdistrict', 'TakBaidistrict', 'ThaMuang']
-    per = ['Unknown', 'Separatists', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'RundaKumpulanKecil(RKK)', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown']
+    date = ['2014-11-27', '2014-11-25', '2014-11-18', '2014-11-17', '2014-11-15', '2014-11-15', '2014-11-15', '2014-11-12', '2014-11-11', \
+            '2014-11-11', '2014-11-10', '2014-11-10', '2014-11-10', '2014-11-05', '2014-11-03', '2014-11-02', '2014-11-02', '2014-11-01']
+    city = ['TanyongTalo', 'Waeng', 'RuamThaiPattana', 'SiSakhon', 'BanBluKa', 'KhokPhodistrict', 'Betongdistrict', 'Aiyoeweng', \
+            'KhaoTum', 'BannangSatadistrict', 'Ruesodistrict', 'ChoengKhiri', 'Wat', 'SungaiPadidistrict', 'Talubo', 'Muangdistrict', 'TakBaidistrict', 'ThaMuang']
+    per = ['Unknown', 'Separatists', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown', \
+           'RundaKumpulanKecil(RKK)', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown']
     fata = ['1', '0', '0', '2', '0', '1', '2', '1', '0', '0', '1', '0', '0', '0', '2', '1', '1', '2']
     inju = ['1', '2', '2', '0', '1', '0', '0', '1', '1', '0', '1', '1', '4', '0', '3', '0', '3', '7']
-    target = ['PrivateCitizens&Property', 'Police,EducationalInstitution', 'Government(General)', 'Private', 'Police', 'EducationalInstitution', 'PrivateCitizens&Property', 'EducationalInstitution', 'Military,EducationalInstitution', 'Police', 'Private', 'PrivateCitizens&Property', 'Military,EducationalInstitution', 'Government(General)', 'Government(General)', 'PrivateCitizens&Property', 'PrivateCitizens&Property', 'Business']
+    target = ['PrivateCitizens&Property', 'Police,EducationalInstitution', 'Government(General)', 'Private', 'Police', \
+              'EducationalInstitution', 'PrivateCitizens&Property', 'EducationalInstitution', 'Military,EducationalInstitution', \
+              'Police', 'Private', 'PrivateCitizens&Property', 'Military,EducationalInstitution', 'Government(General)', 'Government(General)', \
+              'PrivateCitizens&Property', 'PrivateCitizens&Property', 'Business']
     inj = 0
     fat = 0
     list_test = []
@@ -35,12 +45,21 @@ def year_2014_11():
     return all_list
 
 def year_2014_10():
-    date = ['2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-27', '2014-10-27', '2014-10-26', '2014-10-21', '2014-10-15', '2014-10-15', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-08', '2014-10-03', '2014-10-02', '2014-10-01']
-    city = ['BanBuere', 'BanBluka', 'BanTalaKhosator', 'Samakkhi', 'Sawo', 'BangKhao', 'MuangPattanidistrict', 'MuangPattanidistrict', 'MuangPattanidistrict', 'MuangPattanidistrict', 'PuloPuyo', 'DonRak', 'Tabing', 'Ruesodistrict', 'Jakwa', 'SungaiKolokdistrict', 'Mayodistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'Ruesodistrict', 'NongChikdistrict', 'JohBoh', 'Unknown']
-    per = ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Unknown', 'Unknown', 'RundaKumpulanKecil(RKK)']
+    date = ['2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', \
+            '2014-10-31', '2014-10-27', '2014-10-27', '2014-10-26', '2014-10-21', '2014-10-15', '2014-10-15', '2014-10-12', '2014-10-12', '2014-10-12', \
+            '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-08', '2014-10-03', '2014-10-02', '2014-10-01']
+    city = ['BanBuere', 'BanBluka', 'BanTalaKhosator', 'Samakkhi', 'Sawo', 'BangKhao', 'MuangPattanidistrict', 'MuangPattanidistrict', \
+            'MuangPattanidistrict', 'MuangPattanidistrict', 'PuloPuyo', 'DonRak', 'Tabing', 'Ruesodistrict', 'Jakwa', 'SungaiKolokdistrict', 'Mayodistrict', \
+            'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'ThungYangDaengdistrict', 'Ruesodistrict', 'NongChikdistrict', 'JohBoh', 'Unknown']
+    per = ['Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', \
+           'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', 'Separatists', \
+           'Unknown', 'Unknown', 'RundaKumpulanKecil(RKK)']
     fata = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0']
     inju = ['0', '0', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '4', '0', '0', '0', '0', '0', '0', '2', '0', '0', '6']
-    target = ['Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Military', 'Business', 'Business', 'Business', 'Business', 'Government(General)', 'Government(General)', 'Military', 'Police', 'Military', 'Government(General),Military', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'Military,PrivateCitizens&Property', 'Police,EducationalInstitution', 'Military', 'Military,EducationalInstitution']
+    target = ['Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Military', 'Business', \
+              'Business', 'Business', 'Business', 'Government(General)', 'Government(General)', 'Military', 'Police', 'Military', 'Government(General),Military', \
+              'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', \
+              'EducationalInstitution', 'Military,PrivateCitizens&Property', 'Police,EducationalInstitution', 'Military', 'Military,EducationalInstitution']
     inj = 0
     fat = 0
     list_test = []
@@ -52,12 +71,16 @@ def year_2014_10():
     return all_list
 
 def year_2014_09():
-    date = ['2014-09-29', '2014-09-24', '2014-09-19', '2014-09-22', '2014-09-19', '2014-09-12', '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-06']
-    city = ['Talopuyo', 'LanChang', 'MuangYaladistrict', 'Rueso', 'ThanTodistrict', 'Rangaedistrict', 'Thephadistrict', 'Yaringdistrict', 'KhokPhodistrict', 'KhokPhodistrict', 'Batong']
+    date = ['2014-09-29', '2014-09-24', '2014-09-19', '2014-09-22', '2014-09-19', '2014-09-12', \
+            '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-06']
+    city = ['Talopuyo', 'LanChang', 'MuangYaladistrict', 'Rueso', 'ThanTodistrict', 'Rangaedistrict', \
+            'Thephadistrict', 'Yaringdistrict', 'KhokPhodistrict', 'KhokPhodistrict', 'Batong']
     per = ['Unknown', 'Unknown', 'Unknown', 'Separatists', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Separatists', 'Separatists', 'Unknown']
     fata = ['0', '1', '0', '1', '0', '0', '1', '0', '1', '2', '0']
     inju = ['2', '0', '1', '2', '0', '0', '0', '0', '5', '0', '0']
-    target = ['Military,EducationalInstitution', 'Government(General)', 'Police,EducationalInstitution', 'Police,EducationalInstitution', 'Military', 'Transportation', 'PrivateCitizens&Property', 'Government(General)', 'Government(General),Other,PrivateCitizens&Property', 'Government(General)', 'Transportation']
+    target = ['Military,EducationalInstitution', 'Government(General)', 'Police,EducationalInstitution', \
+              'Police,EducationalInstitution', 'Military', 'Transportation', 'PrivateCitizens&Property', 'Government(General)', \
+              'Government(General),Other,PrivateCitizens&Property', 'Government(General)', 'Transportation']
     inj = 0
     fat = 0
     list_test = []
