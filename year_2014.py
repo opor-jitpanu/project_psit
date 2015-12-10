56b7f36ff172ba0fce9146db27d1606cc737e276
opor-jitpanu <opor_tan@hotmail.co.th>
2015-12-11 02:20:59 +0700

    sample
    ปวดหลังมากกกกก

--- year_2014.py
+++ year_2014.py
@@ -8,10 +8,15 @@ def year_2014_12():
     inju = ['0', '0', '0', '0', '3', '0', '2', '0', '1', '0', '0', '0', '2', '0', '0', '0']
     target = ['Police,Police', 'Police', 'PrivateCitizens&Property', 'Police', 'Military', 'Government(General)', 'Police', 'Military,EducationalInstitution', 'Police', 'Government(General)', 'PrivateCitizens&Property', 'Police', 'Business', 'Police', 'Business', 'Government(General)']
     inj = 0
+    fat = 0
+    list_test = []
     for i in inju:
         inj += int(i)
-    return inj
-    
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat]
+    return all_list
+
 def year_2014_11():
     date = ['2014-11-27', '2014-11-25', '2014-11-18', '2014-11-17', '2014-11-15', '2014-11-15', '2014-11-15', '2014-11-12', '2014-11-11', '2014-11-11', '2014-11-10', '2014-11-10', '2014-11-10', '2014-11-05', '2014-11-03', '2014-11-02', '2014-11-02', '2014-11-01']
     city = ['TanyongTalo', 'Waeng', 'RuamThaiPattana', 'SiSakhon', 'BanBluKa', 'KhokPhodistrict', 'Betongdistrict', 'Aiyoeweng', 'KhaoTum', 'BannangSatadistrict', 'Ruesodistrict', 'ChoengKhiri', 'Wat', 'SungaiPadidistrict', 'Talubo', 'Muangdistrict', 'TakBaidistrict', 'ThaMuang']
@@ -19,6 +24,15 @@ def year_2014_11():
     fata = ['1', '0', '0', '2', '0', '1', '2', '1', '0', '0', '1', '0', '0', '0', '2', '1', '1', '2']
     inju = ['1', '2', '2', '0', '1', '0', '0', '1', '1', '0', '1', '1', '4', '0', '3', '0', '3', '7']
     target = ['PrivateCitizens&Property', 'Police,EducationalInstitution', 'Government(General)', 'Private', 'Police', 'EducationalInstitution', 'PrivateCitizens&Property', 'EducationalInstitution', 'Military,EducationalInstitution', 'Police', 'Private', 'PrivateCitizens&Property', 'Military,EducationalInstitution', 'Government(General)', 'Government(General)', 'PrivateCitizens&Property', 'PrivateCitizens&Property', 'Business']
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat]
+    return all_list
 
 def year_2014_10():
     date = ['2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-31', '2014-10-27', '2014-10-27', '2014-10-26', '2014-10-21', '2014-10-15', '2014-10-15', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-12', '2014-10-08', '2014-10-03', '2014-10-02', '2014-10-01']
@@ -27,6 +41,15 @@ def year_2014_10():
     fata = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0']
     inju = ['0', '0', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '4', '0', '0', '0', '0', '0', '0', '2', '0', '0', '6']
     target = ['Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Telecommunication', 'Military', 'Business', 'Business', 'Business', 'Business', 'Government(General)', 'Government(General)', 'Military', 'Police', 'Military', 'Government(General),Military', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'EducationalInstitution', 'Military,PrivateCitizens&Property', 'Police,EducationalInstitution', 'Military', 'Military,EducationalInstitution']
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat]
+    return all_list
 
 def year_2014_09():
     date = ['2014-09-29', '2014-09-24', '2014-09-19', '2014-09-22', '2014-09-19', '2014-09-12', '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-11', '2014-09-06']
@@ -35,18 +58,45 @@ def year_2014_09():
     fata = ['0', '1', '0', '1', '0', '0', '1', '0', '1', '2', '0']
     inju = ['2', '0', '1', '2', '0', '0', '0', '0', '5', '0', '0']
     target = ['Military,EducationalInstitution', 'Government(General)', 'Police,EducationalInstitution', 'Police,EducationalInstitution', 'Military', 'Transportation', 'PrivateCitizens&Property', 'Government(General)', 'Government(General),Other,PrivateCitizens&Property', 'Government(General)', 'Transportation']
-
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat]
+    return all_list
     
 def test():
-    print("enter year or type all for all")
+    print("enter year")
     year = input()
     if year == '2014':
-        print('enter your mont or type all for all')
+        print('enter your mont')
         mont = input()
         if mont == '12':
-            year_2014_12()
-            print(inj)
-            
-            
+            j = year_2014_12()
+            inj = j[0]
+            fata = j[1]
+            print('all in jur in mont =', inj)
+            print('all fata in mont =', fata)
+        elif mont == '10':
+            j = year_2014_10()
+            inj = j[0]
+            fata = j[1]
+            print('all in jur in mont =', inj)
+            print('all fata in mont =', fata)
+        elif mont == '11':
+            j = year_2014_11()
+            inj = j[0]
+            fata = j[1]
+            print('all in jur in mont =', inj)
+            print('all fata in mont =', fata)
+        elif mont == '09':
+            j = year_2014_09()
+            inj = j[0]
+            fata = j[1]
+            print('all in jur in mont =', inj)
+            print('all fata in mont =', fata)
 test()
     
