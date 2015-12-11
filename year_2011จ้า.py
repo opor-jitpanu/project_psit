a26a87cfa32ced6ea4106e5fa3f36569ad86edd9
bankcml <supachai_b_@hotmail.com>
2015-12-12 00:10:48 +0700

    2011 นะ

---
+++ year_2011จ้า.py
@@ -0,0 +1,10 @@
+def year_2011_12():
+    
+    
+ 
+    city = ['Tathong', 'Ban Ton Phai', 'Raman', 'Ban Klang', 'Tambon Katong', 'Tambon Al Yer Weng'\
+            , 'Unknown', 'Unknown', 'Ban Paror Pateh', 'Bacho', 'Unknown', 'Yarang', 'Payud'\
+            , 'Unknown']
+ 
+    fata = ['0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '3', '0', '1', '0']
+    inju = ['1', '2', '0', '1', '1', '1', '22', '0', '3', '0', '0', '1', '0', '0']
