d2a5077a68451225d2233100c48a60310e0fce3c
bankcml <supachai_b_@hotmail.com>
2015-12-12 02:46:40 +0700

    ปี2010 ธค - กย

---
+++ year_2010.py
@@ -0,0 +1,45 @@
+def year_2010_12():
+    
+    
+ 
+    city = ['Muang', 'Unknown', 'Unknown', 'Unknown', 'Ramen', 'Saiburi', 'Muang', 'Bacho'\
+            , 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown']
+    fata = ['0', '0', '1', '2', '1', '1', '2', '1', '0', '1', '1', '1', '1', '1']
+    inju = ['2', '7', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0', '0', '0'] 
+
+def year_2010_11():
+    
+    
+ 
+    city = ['Rusoh', 'Mae Laen', 'Bangkok', 'Yarang', 'Ra-ngae', 'Bannang Sata', 'Din Daeng'\
+            , 'Bacho']
+    fata = ['3', '0', '1', '1', '1', '1', '0', '0']
+    inju = ['0', '1', '9', '0', '0', '0', '0', '2']
+    
+def year_2010_10():
+    
+    
+ 
+    city = ['Khok Pho', 'Raman', 'Raman', 'Raman', 'Kapor', 'Rusoh', 'Yarang', 'Muang', 'Muang'\
+            , 'Pattani', 'Unknown', 'Yarang', 'Yarang', 'Yaha', 'Yaha', 'Chana', 'Saiburi'\
+            , 'Unknown', 'Bang Bua Thong', 'Ratchathewi']
+    fata = ['1', '0', '1', '0', '0', '1', '1', '1', '1', '1', '1', '1', '0', '1', '1', '0'\
+            , '3', '1', '4', '0']
+    inju = ['0', '1', '1', '1', '2', '0', '0', '1', '0', '3', '0', '0', '1', '0', '0', '4'. '0'\
+            , '0', '9', '0']
+
+def year_2010_9():
+    
+    
+ 
+    city = ['Bangkok', 'Saiburi', 'Bangkok', 'Bangkok', 'Bangkok', 'Unknown', 'Muang', 'Bacho'\
+            , 'Khok Pho', 'Unknown', 'Raman', 'Chiang Mai', 'Krong Pinang', 'Raman', 'Ra-ngae'\
+            , 'Rusoh', 'Raman', 'Yi Ngo', 'Yi Ngo', 'Sungai Padi', 'Ra-ngae', 'Muang', 'Muang'\
+            , 'Muang', 'Chanae', 'Chanae', 'Bacho', 'Bacho', 'Bacho', 'Bacho', 'Bacho', 'Bacho'\
+            , 'Khok Pho', 'Takbai', 'Yarang', 'Pattani', 'Khok Pho']
+    fata = ['0', '5', '0', '0', '0', '2', '0', '4', '0', '2', '1', '0', '0', '0', '1', '0', '1'\
+            , '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'\
+            , '1', '0', '2']
+    inju = ['0', '3', '0', '0', '3', '0', '2', '0', '2', '0', '0', '0', '0', '2', '1', '3', '3'\
+            , '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '4', '0'\
+            , '1', '0', '0']
