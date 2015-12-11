0d4050e190bbfd468fce2df1cb43d4ad7026279f
bankcml <supachai_b_@hotmail.com>
2015-12-12 01:23:01 +0700

    dec to june 2011

--- year_2011จ้า.py
+++ year_2011จ้า.py
@@ -39,3 +39,55 @@ def year_2011_10():
             , '2', '2', '3', '3', '3', '0', '1']
     inju = ['0', '0', '2', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3'\
             , '5', '3', '4', '1', '0', '1', '0']
+
+def year_2011_9():
+    
+    
+ 
+    city = ['Bacho', 'Unknown', 'Sungai Kolok', 'Sungai Kolok', 'Sungai Kolok', 'Muang', 'Rusoh'] 
+
+
+ 
+    fata = ['1', '2', '2', '1', '1', '0', '1']
+    inju = ['0', '3', '40', '39', '39', '5', '0']
+
+def year_2011_8():
+    
+    
+ 
+    city = ['Than To', 'Rusoh', 'Yala', 'Yala', 'Saiburi', 'Pattani', 'Yala', 'Ra-ngae', 'Nong Chik'\
+            , 'Yala', 'Saiburi', 'Nong Chik']
+
+
+ 
+    fata = ['0', '1', '0', '2', '1', '0', '1', '1', '1', '0', '2', '1']
+    inju = ['4', '0', '0', '10', '0', '12', '0', '0', '0', '2', '0', '0']
+
+def year_2011_7():
+    
+    
+ 
+    city = ['Rusoh', 'Ra-ngae', 'Muang', 'Yaling', 'Unknown', 'Raman', 'Raman', 'Muang', 'Ra-ngae'\
+            , 'Ra-ngae', 'Muang', 'Bacho']
+
+
+ 
+    fata = ['0', '0', '0', '0', '2', '3', '0', '0', '2', '1', '0']
+    inju = ['1', '0', '7', '1', '11', '0', '10', '0', '0', '0', '0']
+
+def year_2011_6():
+    
+    
+ 
+    city = ['Takbai', 'Bannang Sata', 'Raman', 'Yala', 'Yarang', 'Khok Pho', 'Sungai Pad', 'Cho I Rong'\
+            , 'Cho I Rong', 'Takbai']
+
+
+ 
+    fata = ['2', '1', '1', '1', '0', '1', '1', '1', '1', '0']
+    inju = ['10', '0', '0', '1', '1', '0', '1', '5', '0', '0']
+
+
+
+
+
