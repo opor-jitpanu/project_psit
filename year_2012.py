48991562bf52007fb038f0cd04739a19c5402409
man742141189 <s8070127@kmitl.ac.th>
2015-12-11 21:48:50 +0700

    year_2012_11

--- year_2012.py
+++ year_2012.py
@@ -10,10 +10,20 @@ def year_2012_12():
 
 
 def year_2012_11():
-    city =['Bacho district','Bacho district','Bacho district']
-    fata = ['0','0','0']
-    inju = ['0','0','0']
-    target =['Police,Police','Police','Private Citizens & Property']
+    city =['Mai Kaen','Panare','Yarang','Bannang Sata','Nong Chik','Khok Pho','Rueso','Rueso','Rueso','Rueso','Mueang Yala','Unknown','Chanae district','Sungai Padi','Yaha','Takua Thung','Yarang','Khok Pho','Rueso','Rueso','Sungai Padi','Chanae','Chanae','Yarang','Mayo','Rueso','Rueso']
+    fata = ['1','0','1','0','1','0','0','0','0','3','1','1','1','1','1','1','0','0','0','3','1','0','0','0','1','3','0']
+    inju = ['5','0','0','3','0','2','3','0','0','36','33','0','0','0','0','0','0','6','7','0','0','0','0','1','0','Unkown','8']
+    target =['Educational Institution','Police','Government (General)','Military','Educational Institution','Military','Military,Private Citizens & Property','Religious Figures/Institutions','Military','Transportation','Military','Military,Private Citizens & Property','Business','Government (General)','Government (General)','Religious Figures/Institutions','Government (General)','Educational Institution','Police,Private Citizens & Property','Private Citizens & Property','Police,Private Citizens & Property','Business','Private Citizens & Property','Poilce','Educational Institution','Private Citizens & Property','Police,Private Citizens & Property','Educational Institution']
+
+
+
+def year_2012_10():
+    city =['Mai Kaen','Panare','Yarang','Bannang Sata','','','','','','','','','','','','','','','','','','','']
+    fata = ['1','0','1','0','','','','','','','','','','','','','','','']
+    inju = ['5','0','0','3','','','','','','','','','','','','','','','']
+    target =['Educational Institution','Police','Government (General)','','','','','','','','','','','','','','','']
+
+
 
 
 
