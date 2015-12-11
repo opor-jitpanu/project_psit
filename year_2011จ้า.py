5ce8fc8bd89e07f124bd3e68bf816c5403a764fe
bankcml <supachai_b_@hotmail.com>
2015-12-12 00:56:09 +0700

    เพิ่มเดือนพฤศจิกา ตุลา

--- year_2011จ้า.py
+++ year_2011จ้า.py
@@ -4,7 +4,38 @@ def year_2011_12():
  
     city = ['Tathong', 'Ban Ton Phai', 'Raman', 'Ban Klang', 'Tambon Katong', 'Tambon Al Yer Weng'\
             , 'Unknown', 'Unknown', 'Ban Paror Pateh', 'Bacho', 'Unknown', 'Yarang', 'Payud'\
-            , 'Unknown']
+            , 'Unknown', 'Unknown', 'Unknown', 'Ban Buathong', 'Bangkok']
+
+    
+    fata = ['0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '3', '0', '1', '0', '0', '0', '1', '0']
+    inju = ['1', '2', '0', '1', '1', '1', '22', '0', '3', '0', '0', '1', '0', '0', '1', '1', '1', '0']
+    
+def year_2011_11():
+    
+    
  
-    fata = ['0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '3', '0', '1', '0']
-    inju = ['1', '2', '0', '1', '1', '1', '22', '0', '3', '0', '0', '1', '0', '0']
+    city = ['Unknown', 'Ban Pungu', 'Manang Tayor', 'Tambon Wang Phraya' , 'Unknown', 'Unknown'\
+            , 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Pattani', 'Unknown'\
+            , 'Pattani', 'Unknown', 'Ban Pileng', 'Palo Bata', 'Unknown', 'Unknown', 'Ban Kayumati']
+
+
+ 
+    fata = ['1', '3', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'\
+            , '2', '0', '0', '6', '0', '0']
+    inju = ['0', '0', '1', '0', '9', '0', '0', '0', '0', '0', '0', '0', '1', '0'\
+            , '0', '1', '6', '1', '5', '0'] 
+
+def year_2011_10():
+    
+    
+ 
+    city = ['Takbai', 'Sungai Kolok', 'Narathiwat', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala'\
+            , 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala'\
+            , 'Yala', 'Muang', 'Muang', 'Mayo', 'Panareh', 'Yala', 'Chanae']
+
+
+ 
+    fata = ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'\
+            , '2', '2', '3', '3', '3', '0', '1']
+    inju = ['0', '0', '2', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3'\
+            , '5', '3', '4', '1', '0', '1', '0']
