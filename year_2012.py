652b9de7cdd0ce9769df5dd9d7282c7f3f787244
man742141189 <s8070127@kmitl.ac.th>
2015-12-12 15:14:38 +0700

    year_2012_05

--- year_2012.py
+++ year_2012.py
@@ -1,5 +1,5 @@
 def year_2012_12():
-    city =['See Khiw district','Rangae','Krong Pinang','Unknown','Cho Airong','Cho Airong','Mayo','Rangae','Yaring','Yaring','Sungai Padi','Unknown','Sai Buri','Panare']
+    city = ['See Khiw district','Rangae','Krong Pinang','Unknown','Cho Airong','Cho Airong','Mayo','Rangae','Yaring','Yaring','Sungai Padi','Unknown','Sai Buri','Panare']
     fata = ['1','0','1','0','2','0','2','4','0','0','0','1','1','0']
     inju = ['0','1','0','0','5','6','0','4','0','2','1','0','0','0']
     for i in inju:
@@ -8,42 +8,45 @@ def year_2012_12():
 
 
 def year_2012_11():
-    city =['Mai Kaen','Panare','Yarang','Bannang Sata','Nong Chik','Khok Pho','Rueso','Rueso','Rueso','Rueso','Mueang Yala','Unknown','Chanae district','Sungai Padi','Yaha','Takua Thung','Yarang','Khok Pho','Rueso','Rueso','Sungai Padi','Chanae','Chanae','Yarang','Mayo','Rueso','Rueso']
+    city = ['Mai Kaen','Panare','Yarang','Bannang Sata','Nong Chik','Khok Pho','Rueso','Rueso','Rueso','Rueso','Mueang Yala','Unknown','Chanae district','Sungai Padi','Yaha','Takua Thung','Yarang','Khok Pho','Rueso','Rueso','Sungai Padi','Chanae','Chanae','Yarang','Mayo','Rueso','Rueso']
     fata = ['1','0','1','0','1','0','0','0','0','3','1','1','1','1','1','1','0','0','0','3','1','0','0','0','1','3','0']
     inju = ['5','0','0','3','0','2','3','0','0','36','33','0','0','0','0','0','0','6','7','0','0','0','0','1','0','0','8']
 
 
 def year_2012_10():
-    city =['Yaha','Sai Buri','Sai Buri','Yarang','Bang Por','Sai Buri','Si Sakhon','Rueso','Bare Tai','Bare Tai','Khosit','Tak Bai district','Tak Bai district','Tak Bai district','Tak Bai district','Tak Bai district','Rangae','Krong Pinang','Kabang','Lam Luk Ka','Rangae','Sai Buri','Rangae','Rueso', 'BanAiYoeBaro', 'BanAiYoeBaro', 'Unknown', 'Unknown', 'Unknown', 'Rangae', 'Yarangdistrict', 'KhokPho', 'SiSakhon', 'Yaha', 'Yarang', 'Yala', 'Rangae']
+    city = ['Yaha','Sai Buri','Sai Buri','Yarang','Bang Por','Sai Buri','Si Sakhon','Rueso','Bare Tai','Bare Tai','Khosit','Tak Bai district','Tak Bai district','Tak Bai district','Tak Bai district','Tak Bai district','Rangae','Krong Pinang','Kabang','Lam Luk Ka','Rangae','Sai Buri','Rangae','Rueso', 'BanAiYoeBaro', 'BanAiYoeBaro', 'Unknown', 'Unknown', 'Unknown', 'Rangae', 'Yarangdistrict', 'KhokPho', 'SiSakhon', 'Yaha', 'Yarang', 'Yala', 'Rangae']
     fata = ['1','1','0','1','1','0','0','1','1','0','0','0','0','1','0','0','0','1','0','1','0', '0', '2', '2', '3', '2', '3', '2', '0', '0', '2', '0', '1']
     inju = ['0','2','0','0','0','9','0','0','5','3','0','1','0','1','1','1','0','0','1','0','0', '9', '0', '0', '0', '0', '0', '0', '0', '3', '0', '0', '0']
 
 
 def year_2012_09():
-    city =['Rueso district','Bacho district','Pohon Jerai','Raman district','Raman district','Yingodistrict', 'Bachodistrict', 'BanRi-Ngae', 'SaiBuri', 'SaiBuri', 'SatengNok', 'SatengNok', 'Kalisa', 'BangoSato', 'Bachodistrict', 'Lamphu', 'BanJohwa', 'BanJohwa', 'BangNak', 'BanKaena', 'Tanyongmat']
+    city = ['Rueso district','Bacho district','Pohon Jerai','Raman district','Raman district','Yingodistrict', 'Bachodistrict', 'BanRi-Ngae', 'SaiBuri', 'SaiBuri', 'SatengNok', 'SatengNok', 'Kalisa', 'BangoSato', 'Bachodistrict', 'Lamphu', 'BanJohwa', 'BanJohwa', 'BangNak', 'BanKaena', 'Tanyongmat']
     fata = ['0','1','0','2','0','0', '0', '0', '0', '6', '0', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0']
     inju = ['0','23','4','0','0','2', '4', '1', '0', '40', '0', '0', '1', '3', '1', '4', '0', '0', '5', '1', '1']
 
 def year_2012_08():
-    city =['Muang Narathiwat','Bannang Sata','Raman district','Cho-airong district','Chalerm','Chalerm','Chalerm','Nong Chik district','Yarang district','Panare','Bo Thong','Tuyong','Tuyong','Tuyong','Bang Khao','Bang Khao','Khok Pho','Khok Pho','Sai Buri district','Suwari','Ban To Deng','Sungai Padi district','Korlortanyong','Khok Pho','Ban Ba Ngo Mu long','Kawa','Khao Tum','Tak Bai district','Pase Yawo','Mayo district']
+    city = ['Muang Narathiwat','Bannang Sata','Raman district','Cho-airong district','Chalerm','Chalerm','Chalerm','Nong Chik district','Yarang district','Panare','Bo Thong','Tuyong','Tuyong','Tuyong','Bang Khao','Bang Khao','Khok Pho','Khok Pho','Sai Buri district','Suwari','Ban To Deng','Sungai Padi district','Korlortanyong','Khok Pho','Ban Ba Ngo Mu long','Kawa','Khao Tum','Tak Bai district','Pase Yawo','Mayo district']
     fata = ['0','1','1','1','1','0','1','1','0','0','0','0','0','0','0','0','0','0','1','0','0','0','0','0','0','0','0','0','1','1']
     inju = ['0','0','0','1','1','0','1','0','5','0','0','0','0','0','0','0','0','0','0','0','0','0','3','4','2','0','9','0','0','0']
 
 
 def year_2012_07():
-    city =['MuangPattani', 'ChoAirong', 'ThungYangDaeng', 'Mueang', 'Rueso', 'Mayo', 'Rueso', 'Raman', 'Raman', 'Rangae', 'Muang', 'Rangae', 'SungaiKolok', 'Unknown', 'KhokPho', 'Rueso', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'ThungYangDaeng', 'Kapho', 'Kapho', 'Rangae', 'Chuap', 'Ruesodistrict', 'Rueso', 'BanMo', 'BanKuelong', 'Raman']
+    city = ['MuangPattani', 'ChoAirong', 'ThungYangDaeng', 'Mueang', 'Rueso', 'Mayo', 'Rueso', 'Raman', 'Raman', 'Rangae', 'Muang', 'Rangae', 'SungaiKolok', 'Unknown', 'KhokPho', 'Rueso', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'BannangSata', 'ThungYangDaeng', 'Kapho', 'Kapho', 'Rangae', 'Chuap', 'Ruesodistrict', 'Rueso', 'BanMo', 'BanKuelong', 'Raman']
     fata = ['0', '2', '1', '1', '0', '4', '0', '1', '5', '0', '0', '0', '0', '1', '1', '5', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1']
     inju = ['3', '0', '0', '0', '1', '2', '3', '0', '1', '2', '2', '8', '8', '1', '1', '5', '1', '1', '1', '0', '0', '0', '1', '3', '0', '0', '0', '3', '12', '6', '5', '1', '0']
 
 def year_2012_06():
-    city =['PaseYawo', 'Phraiwan', 'Kaphodistrict', 'KrongPinangdistrict', 'Ramandistrict', 'Sukhirindistrict', 'Ramandistrict', 'SiSakhondistrict', 'KrongPinangdistrict', 'Chanaedistrict', 'ThungYangDaengdistrict', 'Yarang', 'PorMing', 'RusaMilae', 'SungaiPadi', 'Kaphodistrict', 'Prajan', 'PakaHarang']
+    city = ['PaseYawo', 'Phraiwan', 'Kaphodistrict', 'KrongPinangdistrict', 'Ramandistrict', 'Sukhirindistrict', 'Ramandistrict', 'SiSakhondistrict', 'KrongPinangdistrict', 'Chanaedistrict', 'ThungYangDaengdistrict', 'Yarang', 'PorMing', 'RusaMilae', 'SungaiPadi', 'Kaphodistrict', 'Prajan', 'PakaHarang']
     fata = ['2', '0', '3', '1', '1', '0', '0', '1', '3', '0', '2', '0', '1', '0', '2', '0', '0', '1']
     inju = ['2', '0', '3', '1', '1', '0', '0', '1', '3', '0', '2', '0', '1', '0', '2', '0', '0', '1']
 
 def year_2012_05():
-    city =[]
-    fata = []
-    inju = []
+    city = ['Bacho', 'Kapho', 'Raman', 'Raman', 'BannangSata', 'SaiBuri', 'Unknown', 'Mueang', 'Kapho', 'Rueso', 'Muang', 'Unknown', 'SaiBuri', 'Chanaedistrict', 'Panare', 'Maelan']
+    fata = ['0', '3', '0', '0', '0', '1', '0', '0', '0', '2', '0', '2', '4', '1', '0', '2']
+    inju = ['2', '4', '0', '0', '3', '0', '6', '17', '0', '1', '2', '0', '0', '1', '1', '6']
+
+
+
 
 
 def year_2012_04():
