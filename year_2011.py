bf0eb60c7b4ca94cecd6c73ceef17da11b626b3f
opor-jitpanu <opor_tan@hotmail.co.th>
2015-12-12 23:34:59 +0700

    edit

--- year_2011.py
+++ year_2011.py
@@ -1,153 +1,204 @@
 def year_2011_12():
-    
-    
- 
     city = ['Tathong', 'Ban Ton Phai', 'Raman', 'Ban Klang', 'Tambon Katong', 'Tambon Al Yer Weng'\
             , 'Unknown', 'Unknown', 'Ban Paror Pateh', 'Bacho', 'Unknown', 'Yarang', 'Payud'\
             , 'Unknown', 'Unknown', 'Unknown', 'Ban Buathong', 'Bangkok']
-
-    
     fata = ['0', '0', '1', '0', '1', '0', '0', '0', '1', '0', '3', '0', '1', '0', '0', '0', '1', '0']
     inju = ['1', '2', '0', '1', '1', '1', '22', '0', '3', '0', '0', '1', '0', '0', '1', '1', '1', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
     
 def year_2011_11():
-    
-    
- 
     city = ['Unknown', 'Ban Pungu', 'Manang Tayor', 'Tambon Wang Phraya' , 'Unknown', 'Unknown'\
             , 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Pattani', 'Unknown'\
             , 'Pattani', 'Unknown', 'Ban Pileng', 'Palo Bata', 'Unknown', 'Unknown', 'Ban Kayumati']
-
-
- 
     fata = ['1', '3', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'\
             , '2', '0', '0', '6', '0', '0']
     inju = ['0', '0', '1', '0', '9', '0', '0', '0', '0', '0', '0', '0', '1', '0'\
-            , '0', '1', '6', '1', '5', '0'] 
+            , '0', '1', '6', '1', '5', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
 def year_2011_10():
-    
-    
- 
     city = ['Takbai', 'Sungai Kolok', 'Narathiwat', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala'\
             , 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala', 'Yala'\
             , 'Yala', 'Muang', 'Muang', 'Mayo', 'Panareh', 'Yala', 'Chanae']
-
-
- 
     fata = ['0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'\
             , '2', '2', '3', '3', '3', '0', '1']
     inju = ['0', '0', '2', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3', '3'\
             , '5', '3', '4', '1', '0', '1', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_9():
-    
-    
- 
-    city = ['Bacho', 'Unknown', 'Sungai Kolok', 'Sungai Kolok', 'Sungai Kolok', 'Muang', 'Rusoh'] 
-
-
- 
+def year_2011_09():
+    city = ['Bacho', 'Unknown', 'Sungai Kolok', 'Sungai Kolok', 'Sungai Kolok', 'Muang', 'Rusoh']
     fata = ['1', '2', '2', '1', '1', '0', '1']
     inju = ['0', '3', '40', '39', '39', '5', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_8():
-    
-    
- 
+def year_2011_08():
     city = ['Than To', 'Rusoh', 'Yala', 'Yala', 'Saiburi', 'Pattani', 'Yala', 'Ra-ngae', 'Nong Chik'\
             , 'Yala', 'Saiburi', 'Nong Chik']
-
-
- 
     fata = ['0', '1', '0', '2', '1', '0', '1', '1', '1', '0', '2', '1']
     inju = ['4', '0', '0', '10', '0', '12', '0', '0', '0', '2', '0', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_7():
-    
-    
- 
+def year_2011_07():
     city = ['Rusoh', 'Ra-ngae', 'Muang', 'Yaling', 'Unknown', 'Raman', 'Raman', 'Muang', 'Ra-ngae'\
             , 'Ra-ngae', 'Muang', 'Bacho']
-
-
- 
     fata = ['0', '0', '0', '0', '2', '3', '0', '0', '2', '1', '0']
     inju = ['1', '0', '7', '1', '11', '0', '10', '0', '0', '0', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_6():
-    
-    
- 
+def year_2011_06():
     city = ['Takbai', 'Bannang Sata', 'Raman', 'Yala', 'Yarang', 'Khok Pho', 'Sungai Pad', 'Cho I Rong'\
             , 'Cho I Rong', 'Takbai']
-
-
- 
     fata = ['2', '1', '1', '1', '0', '1', '1', '1', '1', '0']
     inju = ['10', '0', '0', '1', '1', '0', '1', '5', '0', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_5():
-    
-    
- 
+def year_2011_05():
     city = ['Saiburi', 'Saiburi', 'Sungai Padi', 'Sungai Padi', 'Yala', 'Unknown', 'Ban Cho Ai Rong'\
             , 'Kapaw', 'Unknown', 'Pattani', 'Bannang Sata', 'Bannang Sata']
-
-
- 
     fata = ['1', '1', '0', '0', '2', '2', '1', '4', '1', '0', '2', '4']
     inju = ['2', '0', '9', '0', '2', '0', '2', '7', '0', '0', '0', '15']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_4():
-    
-    
- 
+def year_2011_04():
     city = ['Ra-ngae', 'Panareh', 'Sungai Padi', 'Rusoh', 'Bannang Sata', 'Janae', 'Muang', 'Nong Chik']
-
-
- 
     fata = ['1', '0', '1', '0', '1', '2', '2', '1']
     inju = ['0', '0', '0', '1', '0', '0', '1', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_3():
-    
-    
- 
+def year_2011_03():
     city = ['Bannang Sata', 'Rusoh', 'Raman', 'Muang', 'Raman', 'Betong', 'Raman', 'Rusoh', 'Unknown'\
             , 'Unknown', 'Muang', 'Thungyangdaeng', 'Si Sakhon', 'Panareh', 'Khok Pho', 'Saiburi']
-
-
- 
     fata = ['0', '1', '1', '1', '1', '1', '1', '2', '1', '1', '1', '1', '0', '1', '1', '0']
     inju = ['1', '0', '1', '0', '0', '1', '0', '3', '1', '0', '0', '0', '2', '1', '2', '1']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_2():
-    
-    
- 
+def year_2011_02():
     city = ['Muang', 'Muang', 'Muang', 'Cho I Rong', 'Muang', 'Muang', 'Ra-ngae', 'Unknown', 'Yala'\
             , 'Muang', 'Yarang', 'Yarang', 'Panareh', 'Nong Chik', 'Muang', 'Muang', 'Muang', 'Muang'\
             , 'Muang', 'Muang', 'Muang']
-
-
- 
     fata = ['1', '0', '0', '1', '0', '0', '1', '3', '1', '2', '0', '1', '5', '1', '0', '0', '0'\
             , '0', '0', '0', '0']
     inju = ['0', '0', '18', '1', '1', '18', '0', '0', '1', '0', '3', '0', '4', '1', '0', '0', '0'\
             , '0', '0', '0', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
 
-def year_2011_1():
-    
-    
- 
+def year_2011_01():
     city = ['Muang', 'Pattani', 'Ra-ngae', 'Unknown', 'Muang', 'Panareh', 'Muang', 'Khok Pho', 'Muang'\
             , 'Unknown', 'Unknown', 'Sungai Kolok', 'Raman', 'Raman', 'Muang', 'Chanae', 'Unknown'\
             , 'Unknown', 'Narathiwat', 'Nam Phong', 'Ra-ngae']
-
-
- 
     fata = ['1', '0', '0', '9', '1', '0', '1', '0', '1', '1', '2', '1', '0', '0', '1', '1', '2', '1'\
             , '1', '0', '1']
     inju = ['0', '5', '1', '2', '0', '1', '0', '2', '0', '0', '0', '0', '2', '1', '0', '0', '0', '0'\
             , '0', '0', '0']
+    fat = 0
+    inj = 0
+    list_all = []
+    for i in fata:
+        fat += int(i)
+    list_all.append(fat)
+    for j in inju:
+        inj += int(j)
+    list_all = [fat,inj]
+    return list_all
+
+
