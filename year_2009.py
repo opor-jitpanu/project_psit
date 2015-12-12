453653a60a7dfe6eb82061d0b24c741e0838b8d1
planettearth <theplanet_earth@hotmail.com>
2015-12-12 15:14:53 +0700

    add year 2009

---
+++ year_2009.py
@@ -0,0 +1,240 @@
+def year_2009_12():
+
+    city = ['Panareh', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Unknown', 'Bacho', 'Raman', 'Pattani', 'Muang', 'Unknown', 'Unknown', 'Unknown', 'Muang', 'Yaring', 'SungaiPadi', 'Yarang', 'Muang', 'SungaiKolok']
+    fata = ['2', '1', '1', '1', '1', '0', '3', '2', '1', '1', '1', '1', '1', '0', '1', '3', '1', '2', '1']
+    inju = ['3', '2', '0', '0', '0', '2', '2', '0', '0', '0', '0', '0', '0', '1', '0', '10', '2', '9', '2']
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_11():
+
+    city = ['Pattani', 'SaiBuri', 'Yala', 'Pattani', 'Narathiwat', 'Narathiwat', 'Pattani', 'Pattani', 'KhokPho', 'NongChik', 'Panare', 'Narathiwat', 'Bangkok', 'Yala', 'Unknown', 'Muang', 'Narathiwat', 'Unknown', 'Unknown', 'Ra-ngae', 'BannangSata', 'Panareh', 'ThanTo', 'Yala', 'Unknown']
+    fata = ['1', '2', '1', '1', '0', '1', '1', '1', '0', '1', '0', '1', '0', '1', '2', '1', '1', '1', '1', '1', '1', '2', '2', '2', '1']
+    inju = ['0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '4', '0', '12', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0']
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_10():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+     inj = 0
+     fat = 0
+     list_test = []
+     for i in inju:
+         inj += int(i)
+     for j in fata:
+         fat += int(j)
+     all_list = [inj,fat,city_max,la,long]
+     return all_list
+
+def year_2009_09():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_08():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_07():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_06():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_05():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_04():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_03():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_02():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+
+def year_2009_01():
+
+    city = []
+    fata = []
+    inju = []
+
+    city_max =
+    la =
+    long = 
+
+    inj = 0
+    fat = 0
+    list_test = []
+    for i in inju:
+        inj += int(i)
+    for j in fata:
+        fat += int(j)
+    all_list = [inj,fat,city_max,la,long]
+    return all_list
+    
